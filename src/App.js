d8a1143ae3c64976788e921ccc9080b6a08f0885
matroskin062 <dima.matroskin062@gmail.com>
2021-02-27 02:16:10 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import styles from './App.module.css';
 import { useEffect, useState } from 'react';
-import WeatherCard from './components/WeatherCard';
+import CurrentWeather from './components/CurrentWeather/CurrentWeather';
 
 const API_KEY = '166f41f09442b35e807cb8bce5b3cfe6';
-const url = `https://api.openweathermap.org/data/2.5/onecall?lat=50.45466&lon=30.5238&exclude=minutely,current,hourly,alerts&units=metric&lang=ru&appid=${API_KEY}`;
+const url = `https://api.openweathermap.org/data/2.5/onecall?lat=50.45466&lon=30.5238&exclude=minutely,hourly,current,alerts&units=metric&lang=ru&appid=${API_KEY}`;
 
 function App() {
-  const [weather, setWeather] = useState();
+  const [forecast, setForecast] = useState();
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(url);
-      const { daily } = await response.json();
-      setWeather(daily);
-    };
     fetchData();
   }, []);
 
+  const fetchData = async () => {
+    const response = await fetch(url);
+    const { daily } = await response.json();
+    setForecast(daily);
+  };
+
   return (
-    <div>
-      {weather && weather.map((el) => <WeatherCard key={el.dt} {...el} />)}
+    <div className={styles.App}>
+      <div className={styles.Forecast}>
+        {forecast &&
+          forecast.map((el) => <CurrentWeather key={el.dt} {...el} />)}
+      </div>
     </div>
   );
 }
